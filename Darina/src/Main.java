edc58c45049407605a2ed185fb266ebdf8151905
darina <bolshakovad07@gmail.com>
2026-09-05 09:46:46 +0300

    Add files via upload

---
+++ Darina/src/Main.java
@@ -0,0 +1,14 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+                double rubles = 30002.7;
+                double rateUSD = 78.5;
+                double dollars = rubles/ rateUSD;
+
+
+                System.out.printf("У вас на счету %.2f рублей. В долларах это %.2f долларов. Так держать!%n",
+                        rubles, dollars);
+            }
+        }
---
+++ Darina/src/Main2.java
@@ -0,0 +1,13 @@
+public class Main2 {
+    public static void main(String[] args) {
+        String weather = "Дождь";
+
+        if(weather.equals("Дождь")) {
+            System.out.println("Беру с собой зонт");
+        }else{
+            System.out.println("Иду на легке");
+        }
+        System.out.println("Люблю гулять!");
+
+    }
+}
