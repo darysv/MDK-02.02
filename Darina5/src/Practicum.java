0756bb89c7bd3c15cba30b78e2a408c10d7e57ed
darina <bolshakovad07@gmail.com>
2026-09-12 08:36:39 +0300

    Add files via upload

---
+++ Darina5/src/Practicum.java
@@ -0,0 +1,26 @@
+public class Practicum {
+    public static void main(String[] args) {
+        BankAccount bankAccount = new BankAccount();
+        bankAccount.setMoneyAmount(1000);
+        System.out.println("Количество денег на счету - " + bankAccount.getMoneyAmount() + " р.");
+        bankAccount.withdrawAll();
+        System.out.println("Количество денег на счету - " + bankAccount.getMoneyAmount() + " р.");
+    }
+}
+
+class BankAccount {
+    private long moneyAmount;
+
+    public long getMoneyAmount() {
+        return moneyAmount;
+    }
+
+    public void setMoneyAmount(long newMoneyAmount) {
+        moneyAmount = newMoneyAmount;
+    }
+
+    public void withdrawAll() {
+        System.out.println("Со счёта снято " + moneyAmount + " р.");
+        moneyAmount = 0;
+    }
+}
